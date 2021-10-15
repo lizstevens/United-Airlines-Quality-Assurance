0bd6feaaaa23aecad54bed48c09046e8fe876c60
Anika Patel <patela60@msu.edu>
2021-10-15 15:18:14 -0400

    General layout of pdf

--- app/src/main/java/edu/msu/steve702/ua_quality_assurance_platform/InProcessActivity.java
+++ app/src/main/java/edu/msu/steve702/ua_quality_assurance_platform/InProcessActivity.java
@@ -11,8 +11,11 @@ import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
 import android.graphics.Paint;
-import android.graphics.pdf.PdfDocument;
+//import android.graphics.pdf.PdfDocument;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.os.Environment;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -24,10 +27,23 @@ import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.firestore.CollectionReference;
 import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.FirebaseFirestore;
+import com.itextpdf.io.image.ImageData;
+import com.itextpdf.io.image.ImageDataFactory;
+import com.itextpdf.kernel.colors.ColorConstants;
+import com.itextpdf.kernel.pdf.PdfDocument;
+import com.itextpdf.kernel.pdf.PdfWriter;
+import com.itextpdf.layout.Document;
+import com.itextpdf.layout.element.Cell;
+import com.itextpdf.layout.element.Image;
+import com.itextpdf.layout.element.Paragraph;
+import com.itextpdf.layout.element.Table;
 
+import java.io.ByteArrayOutputStream;
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.OutputStream;
 
 
 public class InProcessActivity extends AppCompatActivity implements View.OnClickListener {
@@ -169,41 +185,110 @@ public class InProcessActivity extends AppCompatActivity implements View.OnClick
     }
 
     // this function allows user to create a pdf to store locally
-    private void createPdf() {
-        PdfDocument myPdfDocument = new PdfDocument();
-        Paint myPaint = new Paint();
+    private void createPdf() throws FileNotFoundException {
+        String pdfPath = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS).toString();
+        File file = new File(pdfPath, titleEdit.getText() + ".pdf");
 
-        PdfDocument.PageInfo myInfo = new PdfDocument.PageInfo.Builder(1200, 2010, 1).create();
-        PdfDocument.Page myPage = myPdfDocument.startPage(myInfo);
-        Canvas canvas = myPage.getCanvas();
+        OutputStream outputStream = new FileOutputStream(file);
 
-//        canvas.drawBitmap(scaledbtmp, 0, 0, myPaint);
-        canvas.drawText("Technical Operations Quality Assurance", 40, 50, myPaint);
-        canvas.drawText(titleEdit.getText().toString(), 80, 100, myPaint);
-        canvas.drawText(employeeNameEdit.getText().toString(), 120, 100, myPaint);
-        canvas.drawText(partNumberEdit.getText().toString(), 160, 100, myPaint);
-        canvas.drawText(serialNumberEdit.getText().toString(), 200, 100, myPaint);
-        canvas.drawText(nomenclatureEdit.getText().toString(), 240, 100, myPaint);
-        canvas.drawText(taskEdit.getText().toString(), 280, 100, myPaint);
-        canvas.drawText(techSpecificationsEdit.getText().toString(), 320, 100, myPaint);
-        canvas.drawText(toolingEdit.getText().toString(), 360, 100, myPaint);
-        canvas.drawText(shelfLifeEdit.getText().toString(), 400, 100, myPaint);
-        canvas.drawText(traceEdit.getText().toString(), 440, 100, myPaint);
-        canvas.drawText(reqTrainingEdit.getText().toString(), 480, 100, myPaint);
-        canvas.drawText(trainingDateEdit.getText().toString(), 520, 100, myPaint);
+        PdfWriter writer = new PdfWriter(file);
+        PdfDocument pdfDocument = new PdfDocument(writer);
+        Document document = new Document(pdfDocument);
 
-        myPdfDocument.finishPage(myPage);
+        Drawable drawable = getDrawable(R.drawable.united_airlines_quality_assurance_logo);
+        Bitmap bitmap = ((BitmapDrawable)drawable).getBitmap();
+        ByteArrayOutputStream stream = new ByteArrayOutputStream();
+        bitmap.compress(Bitmap.CompressFormat.PNG, 100, stream);
+        byte[] bitmapData = stream.toByteArray();
 
-        File file = new File(getExternalFilesDir("/"), "in-process.pdf");
+        ImageData imageData = ImageDataFactory.create(bitmapData);
+        Image image = new Image(imageData);
+        image.setHeight(80);
+        image.setWidth(500);
 
-        try {
-         myPdfDocument.writeTo(new FileOutputStream(file));
-        } catch (IOException error) {
-         error.printStackTrace();
-        }
+        Paragraph paragraph = new Paragraph("Technical Operations Quality Assurance");
 
-        myPdfDocument.close();
+        float columnWidth[] = {200f, 200f};
+        Table table = new Table(columnWidth);
+
+        // add cell
+        table.addCell(new Cell().setBackgroundColor(ColorConstants.LIGHT_GRAY).add(new Paragraph("Employee Name: ")));
+        table.addCell(employeeNameEdit.getText().toString());
+
+        table.addCell(new Cell().setBackgroundColor(ColorConstants.LIGHT_GRAY).add(new Paragraph("Part Number: ")));
+        table.addCell(partNumberEdit.getText().toString());
+
+        table.addCell(new Cell().setBackgroundColor(ColorConstants.LIGHT_GRAY).add(new Paragraph("Serial Number: ")));
+        table.addCell(serialNumberEdit.getText().toString());
+
+        table.addCell(new Cell().setBackgroundColor(ColorConstants.LIGHT_GRAY).add(new Paragraph("Nomeclature: ")));
+        table.addCell(nomenclatureEdit.getText().toString());
+
+        table.addCell(new Cell().setBackgroundColor(ColorConstants.LIGHT_GRAY).add(new Paragraph("Task: ")));
+        table.addCell(taskEdit.getText().toString());
+
+        table.addCell(new Cell().setBackgroundColor(ColorConstants.LIGHT_GRAY).add(new Paragraph("Technical Specifications: ")));
+        table.addCell(techSpecificationsEdit.getText().toString());
+
+        table.addCell(new Cell().setBackgroundColor(ColorConstants.LIGHT_GRAY).add(new Paragraph("Tooling: ")));
+        table.addCell(toolingEdit.getText().toString());
+
+        table.addCell(new Cell().setBackgroundColor(ColorConstants.LIGHT_GRAY).add(new Paragraph("Shelf Life: ")));
+        table.addCell(shelfLifeEdit.getText().toString());
+
+        table.addCell(new Cell().setBackgroundColor(ColorConstants.LIGHT_GRAY).add(new Paragraph("Traceability: ")));
+        table.addCell(traceEdit.getText().toString());
+
+        table.addCell(new Cell().setBackgroundColor(ColorConstants.LIGHT_GRAY).add(new Paragraph("Required Training: ")));
+        table.addCell(reqTrainingEdit.getText().toString());
+
+        table.addCell(new Cell().setBackgroundColor(ColorConstants.LIGHT_GRAY).add(new Paragraph("Date Qualified: ")));
+        table.addCell(trainingDateEdit.getText().toString());
+
+
+        document.add(image);
+        document.add(paragraph);
+        document.add(table);
+
+        document.close();
         Toast.makeText(getApplicationContext(), "PDF Created", Toast.LENGTH_LONG).show();
+
+
+
+//        PdfDocument myPdfDocument = new PdfDocument();
+//        Paint myPaint = new Paint();
+//
+//        PdfDocument.PageInfo myInfo = new PdfDocument.PageInfo.Builder(1200, 2010, 1).create();
+//        PdfDocument.Page myPage = myPdfDocument.startPage(myInfo);
+//        Canvas canvas = myPage.getCanvas();
+//
+////        canvas.drawBitmap(scaledbtmp, 0, 0, myPaint);
+//        canvas.drawText("Technical Operations Quality Assurance", 40, 50, myPaint);
+//        canvas.drawText(titleEdit.getText().toString(), 80, 100, myPaint);
+//        canvas.drawText(employeeNameEdit.getText().toString(), 120, 100, myPaint);
+//        canvas.drawText(partNumberEdit.getText().toString(), 160, 100, myPaint);
+//        canvas.drawText(serialNumberEdit.getText().toString(), 200, 100, myPaint);
+//        canvas.drawText(nomenclatureEdit.getText().toString(), 240, 100, myPaint);
+//        canvas.drawText(taskEdit.getText().toString(), 280, 100, myPaint);
+//        canvas.drawText(techSpecificationsEdit.getText().toString(), 320, 100, myPaint);
+//        canvas.drawText(toolingEdit.getText().toString(), 360, 100, myPaint);
+//        canvas.drawText(shelfLifeEdit.getText().toString(), 400, 100, myPaint);
+//        canvas.drawText(traceEdit.getText().toString(), 440, 100, myPaint);
+//        canvas.drawText(reqTrainingEdit.getText().toString(), 480, 100, myPaint);
+//        canvas.drawText(trainingDateEdit.getText().toString(), 520, 100, myPaint);
+//
+//        myPdfDocument.finishPage(myPage);
+//
+//        File file = new File(getExternalFilesDir("/"), titleEdit.getText() + ".pdf");
+//
+//        try {
+//         myPdfDocument.writeTo(new FileOutputStream(file));
+//        } catch (IOException error) {
+//         error.printStackTrace();
+//        }
+//
+//        myPdfDocument.close();
+//        Toast.makeText(getApplicationContext(), "PDF Created", Toast.LENGTH_LONG).show();
     }
 
 
@@ -217,7 +302,11 @@ public class InProcessActivity extends AppCompatActivity implements View.OnClick
                 clearData();
                 break;
             case R.id.generate_pdf_btn:
-                createPdf();
+                try {
+                    createPdf();
+                } catch (FileNotFoundException e) {
+                    e.printStackTrace();
+                }
                 break;
             case R.id.switch_to_data_tables_btn:
                 startActivity(new Intent(this, TabularDataActivity.class));
