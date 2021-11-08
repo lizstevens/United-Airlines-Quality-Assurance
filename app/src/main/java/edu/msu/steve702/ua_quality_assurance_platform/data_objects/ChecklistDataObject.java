9099ac6bf3de6b28f076d5c14a8d1f756fb520fa
Adeboye Adegbenro <adegben1@cse.msu.edu>
2021-11-07 21:26:10 -0500

    Created data object for checklist

---
+++ app/src/main/java/edu/msu/steve702/ua_quality_assurance_platform/data_objects/ChecklistDataObject.java
@@ -0,0 +1,20 @@
+package edu.msu.steve702.ua_quality_assurance_platform.data_objects;
+
+import java.io.Serializable;
+import java.util.*;
+
+public class ChecklistDataObject implements Serializable {
+
+
+    // Checklist number
+    private String checklist_id;
+
+    // Mapping between
+    private Map<String, >
+
+
+    public ChecklistDataObject(){
+
+    }
+
+}
